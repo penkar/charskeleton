8871ca9494b4a57c4533f82c91d14e8a6e049e3c
Jeffrey Penkar <jspenkar@gmail.com>
2018-12-02 09:55:16 -0600

    State will be preprocessed.

--- src/js/state/CharacterState.js
+++ src/js/state/CharacterState.js
@@ -1,11 +1,33 @@
 import {observable, action} from 'mobx';
 import stateobject from './stateobject';
 
+const preprocessArray = (array) => {
+  return array.map(({data, width, alignment, category, maptype, standardwidth}, index) => {
+    switch (maptype) {
+      case 'InputFieldContainer':
+      default:
+        data = data.map(({placeholder, value, disabled, classes, id, suggestions, raiseLabel}, idx) => ({
+          placeholder:placeholder||"",
+          value:value||"",
+          disabled:disabled||false,
+          classes:classes||"",
+          id:id||`${category}_${idx}`,
+          suggestions:suggestions||[],
+          raiseLabel:raiseLabel||true,
+          classes:classes||""
+        }));
+        break;
+    }
+    return ({data, width, alignment, category, maptype, standardwidth});
+  })
+
+}
+
 class CharacterState {
   @observable name:String = "";
   @observable characterInfo:Array = [];
   constructor() {
-    this.characterInfo = stateobject;
+    this.characterInfo = preprocessArray(stateobject);
   }
 
   @action update = (value, idx, category) => {
