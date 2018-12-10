d4bd3f4383822f974836b99329fad8fc78608d23
Jeffrey Penkar <jspenkar@gmail.com>
2018-12-09 21:21:59 -0600

    RadioInput button added.

---
+++ src/js/lib/VariableInput/RadioInput.react.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import cn from 'classnames';
+import CharacterState from '../../state/CharacterState';
+export default class RadioInput extends React.Component {
+  constructor(props) {super(props);this.state={checked:true}}
+  render() {
+    return (
+      <div className="RadioInput">
+        <div className="label">
+          <i className={cn("material-icons", {checked:!this.props.value})}>radio_button_unchecked</i>
+          <i className={cn("material-icons", {checked: this.props.value})}>radio_button_checked</i>
+        </div>
+        <div className="component" onClick={()=>this.setState({checked: !this.state.checked})}></div>
+      </div>
+    )
+  }
+  _onClick = () => {
+    const value = !this.props.value, {idx, category, field} = this.props;
+    CharacterState.update(value, idx, category, field)
+  }
+}
